b5c0d5de70e000d927353ec7647ca1b76d461aab
Vinayak S <vinayaksukhalal@gmail.com>
2024-01-21 22:15:26 +0530

    updated

--- components/others/LoginForm.jsx
+++ components/others/LoginForm.jsx
@@ -2,10 +2,20 @@
 
 import Link from "next/link";
 import React from "react";
+import {signIn } from "next-auth/react"
 
 export default function LoginForm() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleGoogleSignup =async () => {
+    const result = await signIn("google");
+    if (result?.error) {
+      // Handle sign-in error
+    } else {
+      router.push("/");
+    }
+  };
+
+  const handleGithubSignup = () => {
+    signIn('github');
   };
   return (
     <div className="form-page__content lg:py-50">
@@ -17,56 +27,21 @@ export default function LoginForm() {
               <p className="mt-10">
                 Don't have an account yet?
                 <Link href="/signup" className="text-purple-1">
-                  Sign up for free
+                  Sign up for free  Or sign in using
                 </Link>
               </p>
 
-              <form
-                className="contact-form respondForm__form row y-gap-20 pt-30"
-                onSubmit={handleSubmit}
-              >
-                <div className="col-12">
-                  <label className="text-16 lh-1 fw-500 text-dark-1 mb-10">
-                    Username Or Email
-                  </label>
-                  <input required type="text" name="title" placeholder="Name" />
-                </div>
-                <div className="col-12">
-                  <label className="text-16 lh-1 fw-500 text-dark-1 mb-10">
-                    Password
-                  </label>
-                  <input
-                    required
-                    type="password"
-                    name="title"
-                    placeholder="Password"
-                  />
-                </div>
-                <div className="col-12">
-                  <button
-                    type="submit"
-                    name="submit"
-                    id="submit"
-                    className="button -md -green-1 text-dark-1 fw-500 w-1/1"
-                  >
-                    Login
-                  </button>
-                </div>
-              </form>
-
-              <div className="lh-12 text-dark-1 fw-500 text-center mt-20">
-                Or sign in using
-              </div>
+          
 
               <div className="d-flex x-gap-20 items-center justify-between pt-20">
                 <div>
-                  <button className="button -sm px-24 py-20 -outline-blue-3 text-blue-3 text-14">
-                    Log In via Facebook
+                  <button onClick={handleGoogleSignup} className="button -sm px-24 py-20 -outline-blue-3 text-blue-3 text-14">
+                    Log In via Google
                   </button>
                 </div>
                 <div>
-                  <button className="button -sm px-24 py-20 -outline-red-3 text-red-3 text-14">
-                    Log In via Google+
+                  <button onClick={handleGithubSignup} className="button -sm px-24 py-20 -outline-red-3 text-red-3 text-14">
+                    Log In via GitHub
                   </button>
                 </div>
               </div>
